11e90eb583bdf0e9f7d7c3dca2b05c725708a986
Daniel Klein <danielklein@airpost.net>
2018-08-13 09:34:11 +1000

    Interpolate missing data

--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -5,10 +5,10 @@ export default async function getAvailabilities(date) {
   return [
     {date: new Date('2014-08-10'), slots: []},
     {date: new Date('2014-08-11'), slots: ['9:30', '10:00', '11:30', '12:00']},
-    {slots: []},
-    null,
-    null,
-    null,
-    {date: new Date('2014-08-16')},
-  ]
+    {date: new Date('2014-08-12'), slots: []},
+    {date: new Date('2014-08-13'), slots: []},
+    {date: new Date('2014-08-14'), slots: []},
+    {date: new Date('2014-08-15'), slots: []},
+    {date: new Date('2014-08-16'), slots: []},
+  ];
 }
