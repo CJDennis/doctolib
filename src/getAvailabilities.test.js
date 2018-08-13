c38e08e3c074a325773a68e4d58328d1e4d2938c
Daniel Klein <danielklein@airpost.net>
2018-08-13 17:23:19 +1000

    Add missing semi-colons to unit test

--- src/getAvailabilities.test.js
+++ src/getAvailabilities.test.js
@@ -2,7 +2,7 @@ import knex from 'knexClient'
 import getAvailabilities from './getAvailabilities'
 
 describe('getAvailabilities', () => {
-  beforeEach(() => knex('events').truncate())
+  beforeEach(() => knex('events').truncate());
 
   describe('simple case', () => {
     beforeEach(async () => {
@@ -18,33 +18,33 @@ describe('getAvailabilities', () => {
           starts_at: new Date('2014-08-11 10:30'),
           ends_at: new Date('2014-08-11 11:30'),
         },
-      ])
-    })
+      ]);
+    });
 
     it('should fetch availabilities correctly', async () => {
-      const availabilities = await getAvailabilities(new Date('2014-08-10'))
-      expect(availabilities.length).toBe(7)
+      const availabilities = await getAvailabilities(new Date('2014-08-10'));
+      expect(availabilities.length).toBe(7);
 
       expect(String(availabilities[0].date)).toBe(
         String(new Date('2014-08-10')),
-      )
-      expect(availabilities[0].slots).toEqual([])
+      );
+      expect(availabilities[0].slots).toEqual([]);
 
       expect(String(availabilities[1].date)).toBe(
         String(new Date('2014-08-11')),
-      )
+      );
       expect(availabilities[1].slots).toEqual([
         '9:30',
         '10:00',
         '11:30',
         '12:00',
-      ])
+      ]);
 
-      expect(availabilities[2].slots).toEqual([])
+      expect(availabilities[2].slots).toEqual([]);
 
       expect(String(availabilities[6].date)).toBe(
         String(new Date('2014-08-16')),
-      )
-    })
-  })
-})
+      );
+    });
+  });
+});
