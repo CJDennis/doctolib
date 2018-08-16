568cc3cf7d46a5bb5daea9c7112379ae1cd0157d
Daniel Klein <danielklein@airpost.net>
2018-08-16 11:34:08 +1000

    Alter SQL query to handle edge cases that cross week boundaries Merge SQL queries with a parameter Handle weekly recurring events that cross week boundaries by looping until the event is out of range Make code more generic Combine functions Modularise functions Clean up code

--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -1,90 +1,87 @@
 import moment from 'moment';
 import knex from 'knexClient';
 
-export default async function getAvailabilities(start_date_string) {
-  const date_format = 'YYYY-MM-DD';
-  const time_format = 'H:mm';
-  const date_time_format = 'YYYY-MM-DD HH:mm';
+export default async function getAvailabilities(start_date) {
+  const DATE_FORMAT = 'YYYY-MM-DD';
+  const TIME_FORMAT = 'H:mm';
+  const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm';
 
-  let start_date = moment(start_date_string).startOf('day');
-  let end_date = start_date.clone().add(7, 'days');
-  let end_date_string = end_date.format(date_format);
+  let start_date_moment = moment(start_date).startOf('day');
+  let end_date_moment = start_date_moment.clone().add(7, 'days');
 
   let agenda = initialiseAgenda();
-  let appointment_times = await getAppointmentTimes();
-  await addOpenings();
+  let appointment_times = await getEventTimes('appointment');
+  let opening_times = await getEventTimes('opening');
+  addAvailableOpenings();
 
   return agenda;
 
   function initialiseAgenda() {
     let agenda = [];
     for (let day = 0; day < 7; ++day) {
-      let agenda_day = start_date.clone().add(day, 'days').format(date_format);
+      let agenda_day = start_date_moment.clone().add(day, 'days').format(DATE_FORMAT);
       agenda.push({date: new Date(agenda_day), slots: []});
     }
     return agenda;
   }
 
-  async function getAppointmentTimes() {
-    let appointments = await getAppointments();
-    let appointment_times = {};
+  async function getEventTimes(event_type) {
+    let events = await getEventsOfType(event_type);
 
-    for (let i = 0; i < appointments.length; i++) {
-      let appointment_starts_at = moment(appointments[i].starts_at);
-      let appointment_ends_at = moment(appointments[i].ends_at);
-
-      let period = appointment_ends_at.diff(appointment_starts_at, 'minutes');
-      for (let minutes = 0; minutes < period; minutes += 30) {
-        let appointment = appointment_starts_at.clone().add(minutes, 'minutes');
-        appointment_times[appointment.format(date_time_format)] = true;
+    let times = {};
+    for (let i = 0; i < events.length; i++) {
+      let starts_at = moment(events[i].starts_at);
+      let ends_at = moment(events[i].ends_at);
+      if (!events[i].weekly_recurring) {
+        addToTimes(times, starts_at, ends_at);
       }
-    }
+      else {
+        if (ends_at < start_date_moment) {
+          let weeks_to_add = start_date_moment.diff(ends_at, 'weeks') + 1;
+          starts_at.add(weeks_to_add, 'weeks');
+          ends_at.add(weeks_to_add, 'weeks');
+        }
 
-    return appointment_times;
-  }
-
-  async function getAppointments() {
-    return knex('events').where({kind: 'appointment'}).andWhere((builder) =>
-      builder.where((builder) =>
-        builder.where('starts_at', '>=', start_date_string).where('starts_at', '<', end_date_string)
-      ).orWhere((builder) =>
-        builder.where({weekly_recurring: true}).where('starts_at', '<', end_date_string)
-      )
-    );
-  }
-
-  async function addOpenings() {
-    let openings = await getOpenings();
-
-    for (let i = 0; i < openings.length; i++) {
-      let opening_starts_at = moment(openings[i].starts_at);
-      let opening_ends_at = moment(openings[i].ends_at);
-
-      let days_offset_this_week = (opening_starts_at.diff(start_date, 'days') % 7 + 7) % 7;
-      let opening_date_this_week = start_date.clone().add(days_offset_this_week, 'days');
-      let days_offset = opening_date_this_week.diff(opening_starts_at, 'days');
-
-      let opening_starts_at_this_week = opening_starts_at.clone().add(days_offset, 'days');
-      let opening_ends_at_this_week = opening_ends_at.clone().add(days_offset, 'days');
-
-      let period = opening_ends_at_this_week.diff(opening_starts_at_this_week, 'minutes');
-      for (let minutes = 0; minutes < period; minutes += 30) {
-        let opening = opening_starts_at_this_week.clone().add(minutes, 'minutes');
-        let days = opening.diff(start_date, 'days');
-        if (days < 7 && !appointment_times[opening.format(date_time_format)]) {
-          agenda[days].slots.push(opening.format(time_format));
+        while (starts_at < end_date_moment) {
+          addToTimes(times, starts_at, ends_at);
+          starts_at.add(1, 'week');
+          ends_at.add(1, 'week');
         }
       }
     }
+    return times;
   }
 
-  async function getOpenings() {
-    return knex('events').where({kind: 'opening'}).andWhere((builder) =>
+  async function getEventsOfType(event_type) {
+    return knex('events').where({kind: event_type}).andWhere((builder) =>
       builder.where((builder) =>
-        builder.where('starts_at', '>=', start_date_string).where('starts_at', '<', end_date_string)
+        builder.where('starts_at', '>=', start_date_moment.toDate()).where('starts_at', '<', end_date_moment.toDate())
       ).orWhere((builder) =>
-        builder.where({weekly_recurring: true}).where('starts_at', '<', end_date_string)
+        builder.where('ends_at', '>', start_date_moment.toDate()).where('ends_at', '<=', end_date_moment.toDate())
+      ).orWhere((builder) =>
+        builder.where({weekly_recurring: true}).where('starts_at', '<', end_date_moment.toDate())
       )
     );
+  }
+
+  function addToTimes(times, starts_at, ends_at) {
+    let start_time = moment.max(starts_at, start_date_moment);
+    let end_time = moment.min(ends_at, end_date_moment);
+    let period = end_time.diff(start_time, 'minutes');
+    for (let minutes = 0; minutes < period; minutes += 30) {
+      let time = start_time.clone().add(minutes, 'minutes');
+      times[time.format(DATE_TIME_FORMAT)] = time;
+    }
+  }
+
+  function addAvailableOpenings() {
+    for (let time in opening_times) {
+      if (opening_times.hasOwnProperty(time)) {
+        if (!appointment_times[time]) {
+          let days = Math.floor(opening_times[time].diff(start_date_moment, 'days'));
+          agenda[days].slots.push(opening_times[time].format(TIME_FORMAT));
+        }
+      }
+    }
   }
 }
